3ab492df83a185106f386a07ecc814cc95221a72
rob-cost <robertocostantino3@gmail.com>
2026-03-27 13:56:47 +0100

    Created CheckoutHeader component

--- analog-district/src/App.jsx
+++ analog-district/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
-import { CheckoutPage } from "./pages/CheckoutPage";
+import { CheckoutPage } from "./pages/checkout/CheckoutPage";
 import { OrdersPage } from "./pages/OrdersPage";
 import { TrackingPage } from "./pages/TrackingPage";
 import { Routes, Route } from "react-router";
