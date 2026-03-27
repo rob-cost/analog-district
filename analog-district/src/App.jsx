5f6facb201a427ef6b2500f328f6ed4463007464
rob-cost <robertocostantino3@gmail.com>
2026-03-27 10:20:21 +0100

    Created pages using React-Routers

--- analog-district/src/App.jsx
+++ analog-district/src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
 
 function App() {
-  return <HomePage />;
+  return (
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="checkout" element={<div>Test Checkout Page</div>} />
+    </Routes>
+  );
 }
 
 export default App;
