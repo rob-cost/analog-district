696b242ad5edb2b359f76c23f61332760b94895b
rob-cost <robertocostantino3@gmail.com>
2026-03-31 11:07:23 +0200

    Reapply "Fetched data from backend"
    This reverts commit b3af61970efc98ecb999b7842792af51a40e4829.

--- analog-district/src/pages/HomePage.jsx
+++ analog-district/src/pages/HomePage.jsx
@@ -6,6 +6,13 @@ import checkmarkIcon from "../assets/images/icons/checkmark.png";
 import { products } from "../../starting-code/data/products";
 
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    });
   return (
     <>
       <title>Analog District</title>
