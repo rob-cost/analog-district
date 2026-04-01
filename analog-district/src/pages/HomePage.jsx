f33b6e0086b732f315e86cb2d38030c340b39f20
rob-cost <robertocostantino3@gmail.com>
2026-04-01 11:07:50 +0200

    Got card from backend and displayed quantity

--- analog-district/src/pages/HomePage.jsx
+++ analog-district/src/pages/HomePage.jsx
@@ -6,16 +6,23 @@ import { useEffect, useState } from "react";
 
 export function HomePage() {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then(response => {
       setProducts(response.data);
     });
   }, []);
 
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/cart-items").then(response => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <>
       <title>Analog District</title>
-      <Header />
+      <Header cart={cart} />
       <div className="home-page">
         <div className="products-grid">
           {products.map(product => {
