7191fa2c7daacca06537a227cc85efc134528bdc
rob-cost <robertocostantino3@gmail.com>
2026-04-07 16:22:21 +0200

    Generated HTML for tracking page

--- analog-district/src/pages/TrackingPage.jsx
+++ analog-district/src/pages/TrackingPage.jsx
@@ -1,39 +1,76 @@
-import "./TrackingPage.css";
+import dayjs from "dayjs";
+import axios from "axios";
+
 import { Header } from "../components/Header";
 import { Link } from "react-router";
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
 
-export function TrackingPage() {
+import "./TrackingPage.css";
+
+export function TrackingPage({ cart }) {
+  const [currentOrder, setCurrentOrder] = useState(null);
+  const params = useParams();
+  const orderId = params.orderId;
+  const productId = params.productId;
+
+  useEffect(() => {
+    const fetchOrderProducts = async () => {
+      const response = await axios.get(
+        `/api/orders/${orderId}?expand=products`,
+      );
+      setCurrentOrder(response.data);
+    };
+
+    fetchOrderProducts();
+  }, orderId);
+
+  console.log(currentOrder);
+
+  if (!currentOrder) {
+    return null;
+  }
+
+  const currentProduct = currentOrder.products.find(product => {
+    return product.productId === productId;
+  });
+
   return (
     <>
       <title>Tracking</title>
-      <Header />
-      <div class="tracking-page">
-        <div class="order-tracking">
-          <Link class="back-to-orders-link link-primary" to="/orders">
+      <Header cart={cart} />
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <Link className="back-to-orders-link link-primary" to="/orders">
             View all orders
           </Link>
 
-          <div class="delivery-date">Arriving on Monday, June 13</div>
-
-          <div class="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+          <div className="delivery-date">
+            Arriving on{" "}
+            {dayjs(currentProduct.estimatedDeliveryTimeMs).format(
+              "dddd, MMMM D",
+            )}
           </div>
 
-          <div class="product-info">Quantity: 1</div>
+          <div className="product-info">{currentProduct.product.name}</div>
+
+          <div className="product-info">
+            Quantity: {currentProduct.quantity}
+          </div>
 
           <img
-            class="product-image"
+            className="product-image"
             src="images/products/athletic-cotton-socks-6-pairs.jpg"
           />
 
-          <div class="progress-labels-container">
-            <div class="progress-label">Preparing</div>
-            <div class="progress-label current-status">Shipped</div>
-            <div class="progress-label">Delivered</div>
+          <div className="progress-labels-container">
+            <div className="progress-label">Preparing</div>
+            <div className="progress-label current-status">Shipped</div>
+            <div className="progress-label">Delivered</div>
           </div>
 
-          <div class="progress-bar-container">
-            <div class="progress-bar"></div>
+          <div className="progress-bar-container">
+            <div className="progress-bar"></div>
           </div>
         </div>
       </div>
