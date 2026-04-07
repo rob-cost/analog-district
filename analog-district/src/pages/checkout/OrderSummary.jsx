4fce3b647b7454c4ba71fced0622c9f6a1a722c4
rob-cost <robertocostantino3@gmail.com>
2026-04-07 14:12:13 +0200

    Created CartItemDetaills and DeliveryDate components

--- analog-district/src/pages/checkout/OrderSummary.jsx
+++ analog-district/src/pages/checkout/OrderSummary.jsx
@@ -1,7 +1,6 @@
-import dayjs from "dayjs";
-
-import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./DeliveryOptions";
+import { DeliveryDate } from "./DeliveryDate";
+import { CartItemDetails } from "./CartItemDetails";
 
 export function OrderSummary({ deliveryOptions, cart }) {
   return (
@@ -15,37 +14,10 @@ export function OrderSummary({ deliveryOptions, cart }) {
           );
           return (
             <div key={cartItem.productId} className="cart-item-container">
-              <div className="delivery-date">
-                Delivery date:
-                {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format(
-                  "dddd, MMMM D",
-                )}
-              </div>
+              <DeliveryDate selectedDeliveryOption={selectedDeliveryOption} />
 
               <div className="cart-item-details-grid">
-                <img className="product-image" src={cartItem.product.image} />
-
-                <div className="cart-item-details">
-                  <div className="product-name">{cartItem.product.name}</div>
-                  <div className="product-price">
-                    {" "}
-                    {formatMoney(cartItem.product.priceCents)}
-                  </div>
-                  <div className="product-quantity">
-                    <span>
-                      Quantity:{" "}
-                      <span className="quantity-label">
-                        {cartItem.quantity}
-                      </span>
-                    </span>
-                    <span className="update-quantity-link link-primary">
-                      Update
-                    </span>
-                    <span className="delete-quantity-link link-primary">
-                      Delete
-                    </span>
-                  </div>
-                </div>
+                <CartItemDetails cartItem={cartItem} />
 
                 <DeliveryOptions
                   deliveryOptions={deliveryOptions}
